153bdb259a5a32bd7116171b8e90400e42d2a096
ARYAN DANGWAL <150328665+Aryan-Dangwal@users.noreply.github.com>
2025-07-04 23:49:09 +0530

    Time: 0 ms (100%), Space: 16.4 MB (12.53%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        // empty list
-        if (head == NULL) {
+        if(head == NULL ){
             return head;
         }
-        // non-empty list
-        ListNode* curr = head;
-
-        while (curr != NULL && curr->next != NULL) {
-            if (curr->val == curr->next->val) {
-                ListNode* next = curr->next->next;
-                delete (curr->next);
-                curr->next = next;
-
-            } else {
-                curr = curr->next;
+        ListNode* curr= head;
+        while(curr!=NULL && curr->next!=NULL){
+            if(curr->val==curr->next->val){
+                ListNode* next=curr->next->next;
+                delete(curr->next);
+                curr->next=next;
+            }
+            else{
+                curr=curr->next;
             }
         }
         return head;
+
     }
 };

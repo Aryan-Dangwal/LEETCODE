041e56fc5f251804d05e149435400a1857cfdf4e
ARYAN DANGWAL <150328665+Aryan-Dangwal@users.noreply.github.com>
2025-07-19 22:51:41 +0530

    Time: 14 ms (5.19%), Space: 12.4 MB (5.15%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        string s = to_string(x);
-        int start = 0;
-        int end = s.length() - 1;
-
-        while (start < end) {
-            if (s[start] != s[end]) {
+        if (x < 0) return false; // Negative numbers can't be palindromes
+        
+        // Convert the integer to a vector of digits
+        vector<int> b;
+        int temp = x;
+        while (temp > 0) {
+            b.push_back(temp % 10);
+            temp /= 10;
+        }
+        
+        // Compare digits from both ends
+        int n = b.size();
+        for (int i = 0; i < n / 2; ++i) {
+            if (b[i] != b[n - 1 - i]) {
                 return false;
             }
-            start++;
-            end--;
         }
 
         return true;

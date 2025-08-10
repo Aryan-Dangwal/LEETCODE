3922ea92f58d1be35125286dffaa0a7ad05d8197
ARYAN DANGWAL <150328665+Aryan-Dangwal@users.noreply.github.com>
2025-08-10 18:59:09 +0530

    Time: 3 ms (60.16%), Space: 12 MB (28.8%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+      unordered_map<char,int>freq;
+      for(auto ch:magazine){
+        freq[ch]++;
+      }
+
+    for(auto ch:ransomNote){
+        if(freq[ch]==0){
+            return false;
+        }
+        else{ freq[ch]--;}
+    }
+    return true;
+    }
+};

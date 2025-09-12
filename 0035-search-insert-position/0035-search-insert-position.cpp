0854b515783d9c98809dd626e94db10dab51fa10
ARYAN DANGWAL <150328665+Aryan-Dangwal@users.noreply.github.com>
2025-09-12 20:58:35 +0530

    Time: 0 ms (100%), Space: 13.6 MB (76.76%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -3,10 +3,9 @@ public:
     int searchInsert(vector<int>& nums, int target) {
         int n = nums.size();
 
-        // If target is greater than the last element, insert at the end
+        
         if (target > nums[n - 1]) return n;
 
-        // Check where the target fits between two numbers
         for (int i = 1; i < n; i++) {
             if (nums[i - 1] < target && target <= nums[i]) {
                 return i;

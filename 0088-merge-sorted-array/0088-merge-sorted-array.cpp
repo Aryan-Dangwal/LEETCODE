e55b12177c6db6b40bc1833321f0cc1b7c9eca01
ARYAN DANGWAL <150328665+Aryan-Dangwal@users.noreply.github.com>
2025-09-13 22:05:27 +0530

    Time: 0 ms (100%), Space: 12.2 MB (70.66%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,27 +1,10 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = m - 1;        // last element in nums1’s valid part
-        int j = n - 1;        // last element in nums2
-        int k = m + n - 1;    // last index of nums1 (total size)
-
-        // Merge from the back
-        while (i >= 0 && j >= 0) {
-            if (nums1[i] > nums2[j]) {
-                nums1[k] = nums1[i];
-                i--;
-            } else {
-                nums1[k] = nums2[j];
-                j--;
-            }
-            k--;
+        for (int j = 0, i = m; j<n; j++){
+            nums1[i] = nums2[j];
+            i++;
         }
-
-        // If nums2 still has elements left
-        while (j >= 0) {
-            nums1[k] = nums2[j];
-            j--;
-            k--;
-        }
+        sort(nums1.begin(),nums1.end());
     }
 };
